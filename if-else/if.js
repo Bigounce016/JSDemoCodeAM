a2f9ec878852dc11890fd488b9bb519818a200ba
Cr-06 <colemrose@gmail.com>
2026-01-30 09:04:49 -0800

    if-elseif-else
    added the demo code for the if group

---
+++ if-else/if.js
@@ -0,0 +1,17 @@
+
+let ticketType = "standard"; 
+
+if (ticketType === "VIP") {
+ 
+console.log("Welcome to the VIP Lounge!");
+
+} else if (ticketType === "standard") {
+
+console.log("Enjoy the movie in Theater 1.");
+} else {
+
+// This runs if ticketType is anything else //
+
+console.log("Please get out.");
+}
+
